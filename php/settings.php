a7dd5c6823595a9272ce10375e08fce9cdb9c695
Maarten Lensink <m.lensink@symagic.com>
2019-11-13 01:20:24 +0200

    Settings go?

--- php/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-class settings {
-   static $origin = '*';
-   static $dbhost = 'localhost';
-   static $dbuser = 'username';
-   static $dbpass = 'password';
-   static $dbname = 'OpenCiv';
-}
-?>
