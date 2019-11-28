78b7008e362b161e1be6b1bf136e1a53febc1bb1
Maarten Lensink <m.lensink@symagic.com>
2019-11-28 14:22:21 +0200

    Removing unnecessary 'else'

--- php/login.php
+++ php/login.php
@@ -26,16 +26,15 @@ $userId = (int)$query[0][0];
 // Check password
 if (!password_verify($params->password, $query[0][1])) {
    send_result('Password incorrect');
-} else {
-
-   // Create a new token and store in the database and as a cookie
-   $token = generate_token();
-   $db->execute('INSERT INTO `tokens` (`user_id`, `value`, `user_agent`) VALUES (?, ?, ?)', 'iss', $userId, $token, $_SERVER['HTTP_USER_AGENT']);
-   setcookie('token', $token, $timestamp + 31622400, '/');
-
-   // Set session variables
-   $_SESSION['user_id'] = $userId;
 }
+
+// Create a new token and store in the database and as a cookie
+$token = generate_token();
+$db->execute('INSERT INTO `tokens` (`user_id`, `value`, `user_agent`) VALUES (?, ?, ?)', 'iss', $userId, $token, $_SERVER['HTTP_USER_AGENT']);
+setcookie('token', $token, $timestamp + 31622400, '/');
+
+// Set session variable
+$_SESSION['user_id'] = $userId;
 
 // Report success
 send_result(false);
