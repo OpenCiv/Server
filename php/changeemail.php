6f31cc35dd63ec7b406c8532b2d862c0a57cbac0
Maarten Lensink <m.lensink@symagic.com>
2019-12-27 23:28:12 +0200

    Change e-mail

--- php/changeemail.php
+++ php/changeemail.php
@@ -5,7 +5,13 @@ if (!$params || !$params->email) {
    send_result('Parameter missing', 400);
 }
 
-$query = $db->execute('SELECT name FROM users WHERE id = ?', 'i', $userId);
+// Check if the e-mail address is not in use already
+$query = $db->first('SELECT EXISTS (SELECT * FROM users WHERE email = ?)', 's', $params->email);
+if ($query[0]) {
+   send_result('E-mail address already in use');
+}
+
+$query = $db->first('SELECT name FROM users WHERE id = ?', 'i', $userId);
 $db->execute('UPDATE users SET email = ?, verified = 0 WHERE id = ?', 'si', $params->email, $userId);
 send_verification_email($userId, $params->email, $query[0]);
 send_result(true);
