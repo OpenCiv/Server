0ed3ad019a092bbf06ad51c37a0d87c8bd8ecbb9
Maarten Lensink <m.lensink@symagic.com>
2020-03-10 22:35:57 +0200

    Larger map, unit movement

--- php/move.php
+++ php/move.php
@@ -2,7 +2,7 @@
 require 'init.php';
 get_player();
 
-if (!$params || !$params->id || !$params->direction) {
+if (!$params || !$params->id || !$params->x || !$params->y) {
    send_result('Parameter missing', 400);
 }
 
@@ -18,23 +18,13 @@ if ($query[0] != $playerId) {
 $x = (int)$query[1];
 $y = (int)$query[2];
 
-if (strpos($params->direction, 'up') !== false) {
-   $y--;
+$xDiff = abs($x - $params->x);
+$yDiff = abs($y - $params->y);
+if ($xDiff > 1 || $yDiff > 1 || $xDiff + $yDiff === 0) {
+   send_result(false);
 }
 
-if (strpos($params->direction, 'down') !== false) {
-   $y++;
-}
-
-if (strpos($params->direction, 'left') !== false) {
-   $x--;
-}
-
-if (strpos($params->direction, 'right') !== false) {
-   $x++;
-}
-
-$query = $db->first('SELECT type FROM terrain WHERE game_id = ? AND x = ? AND y = ?', 'iii', $gameId, $x, $y);
+$query = $db->first('SELECT type FROM terrain WHERE game_id = ? AND x = ? AND y = ?', 'iii', $gameId, $params->x, $params->y);
 if (!$query) {
    send_result('Off the map', 400);
 }
@@ -43,6 +33,6 @@ if ($query[0] === 'water') {
    send_result(false);
 }
 
-$query = $db->execute('UPDATE units SET x = ?, y = ? WHERE id = ?', 'iii', $x, $y, $params->id);
+$query = $db->execute('UPDATE units SET x = ?, y = ? WHERE id = ?', 'iii', $params->x, $params->y, $params->id);
 send_result(true);
 ?>
